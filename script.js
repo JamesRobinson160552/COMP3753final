ec3f7642ca29cb79eccc42262012818392ad7c71
159910g <122617604+159910g@users.noreply.github.com>
2023-03-19 17:05:31 -0300

    test

--- script.js
+++ script.js
@@ -13,4 +13,21 @@ function SayHello()
     }
 
     console.log("hello2");
+
+    $.get("server.php?action=getcustomers", function(data)  
+    {
+
+        //iterate over the JSON response, building an HTML string
+        var html_string = "";
+            
+        $(data).each(function(key, object) {
+     
+            console.log(object['MediumID']);
+    
+        });
+    
+        //set the HTML string on the client
+        //$("#table_body").html(html_string);
+    
+    });
 }
--- server.php
+++ server.php
@@ -1,5 +1,6 @@
 <?php
-$con = odbc_connect("comp3753-db-zjbr", "Postgres", "123456789");
+$con = odbc_connect("comp3753-db-zjbr.cxbac3bydx0a.us-east-2.rds.amazonaws.com1", "Postgres", "123456789");
+
 
 if(!$con)
 {
@@ -10,4 +11,32 @@ else
 {
     echo "im here";
 }
+
+$action = $_GET['action'];
+
+if ($action === 'getcustomers') //gets all customer data from customer table
+{
+    $odbc_statement = "SELECT MediumID FROM Medium;";
+}
+
+$result = Odbc.Query($con, $odbc_statement); //all information is given to $result
+
+header('Content-Type: application/json; charset=utf-8');
+
+if ($result != null) 
+{
+    //while($row = $result->fetch_array(MYSQLI_ASSOC)) 
+    //{
+    //        $myArray[] = $row;
+    //}
+    //echo json_encode($myArray);
+    echo json_encode($result);
+} 
+else 
+{
+    echo json_encode("'success':false}");
+}
+
+//close the connection
+$con->close();
 ?>
